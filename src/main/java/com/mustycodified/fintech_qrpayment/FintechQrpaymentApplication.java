158c4e9fe26cc55085fa1a6d1ac170b99ecb1f48
Mustapha Musa <mustapha.musa@decagon.dev>
2025-02-20 08:10:49 +0100

    first commit

---
+++ src/main/java/com/mustycodified/fintech_qrpayment/FintechQrpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.mustycodified.fintech_qrpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechQrpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechQrpaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustycodified/fintech_qrpayment/FintechQrpaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustycodified.fintech_qrpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechQrpaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
